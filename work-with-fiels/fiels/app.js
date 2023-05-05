f62ef01c817b326fb06220d9a9d87f92314c6f3e
Sergey Opanasenko <svopanasenko22@gmail.com>
2023-05-06 00:05:50 +0200

    finish 1 part

--- work-with-fiels/fiels/app.js
+++ work-with-fiels/fiels/app.js
@@ -1,7 +1,21 @@
 const { error } = require("console");
 const fs = require("fs");
 
-fs.readFile("./fiels/file.txt", (error, date) => {
-    console.log(error);
-    console.log(data);
-});
+const func = async () => {
+    // const buffer = await fs, readFile("./file.txt")
+    // const text = buffer.toString();
+    // console.log(text);
+    // const text = await fs.readFile("./file.txt", "utf-8");
+    // console.log(text);
+    // const result = await fs.appendFile("./file.txt", "\nДописуем слово");
+    // console.log(result);
+    const result = await fs.writeFile("./file.txt", "Callback hell");
+    console.log(result);
+}
+
+func();
+
+// fs.readFile("./fiels/file.txt", (error, date) => {
+//     console.log(error);
+//     console.log(data);
+// });
